1ac74b6ee03726b16f0e987f2988b2a6fca1822a
MeshVoid <meshvoid@gmail.com>
2022-08-02 16:55:32 +0600

    game window is running, major classes established

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import sys
 import pygame
 
+from settings import GameSettings
+from player import Player
+
+
 class RocketGame():
-    """Rocket game class"""
+    """Rocket Game class to manage game assets and game logic."""
+
     def __init__(self):
-        pass
+        """Initialize the game and initial settings/resources."""
+        pygame.init()
+        self.settings = GameSettings()
+        self.player = Player()
+
+        self._set_windowed_mode()
+
+        pygame.display.set_caption('Rocket Game')
+    
+    def start_game(self):
+        """Start the main loop of the game."""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+    def _set_windowed_mode(self):
+        """Run the game in a window."""
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,
+             self.settings.screen_width))
+
+if __name__== "__main__":
+    # Create an instance of a game and run the game.
+    game = RocketGame()
+    game.start_game()
---
+++ settings.py
@@ -0,0 +1,7 @@
+import pygame
+
+class GameSettings():
+    """Class to store all game settings"""
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 600
