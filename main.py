bcdf2064b96ef3317c915dd39c98fa62a4919696
MeshVoid <meshvoid@gmail.com>
2022-09-20 08:35:57 +0600

    BEEP-boop

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 from settings import GameSettings
 from player import Player
 from projectile import Projectile
-
+from star import Star
 
 class RocketGame():
     """Rocket Game class to manage game assets and game logic."""
@@ -18,13 +18,17 @@ class RocketGame():
         self.player = Player(self)
         self.projectiles = pygame.sprite.Group()
 
+        # Spawn stars
+        self.stars = pygame.sprite.Group()
+        self._create_star_cloud()
+
     def game_loop(self):
         """Start the main loop of the game."""
         while True:
             self._check_events()
+            self.player.update()
             self._update_window()
             self._update_projectile()
-            self.player.update()
 
     def _check_events(self):
         """Check for keypresses and mouse events."""
@@ -88,6 +92,8 @@ class RocketGame():
         self.player.blitme()
         for projectile in self.projectiles.sprites():
             projectile.draw_projectile()
+        # Draw stars
+        self.stars.draw(self.screen)
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
@@ -97,6 +103,34 @@ class RocketGame():
             (self.settings.screen_width,
              self.settings.screen_width))
 
+    def _create_star_cloud(self):
+        """Create the cloud of stars."""
+        #Make an star
+        star = Star(self)
+        star_width, star_height = star.rect.size
+        available_space_x = self.settings.screen_width - (1 * star_width)
+        number_stars_x = int(available_space_x // (1.5 * star_width))
+
+        # Determine the number of rows of stars that fit on the screen
+        player_height = self.player.rect.height
+        available_space_y = (self.settings.screen_height -
+                            ( 2* star_height)- player_height)
+        number_rows = available_space_y // ( star_height)
+
+        # Create the full grid of stars.
+        for row_number in range(number_rows):
+            for star_number in range(number_stars_x):
+                self._create_star(star_number, row_number)
+    
+    def _create_star(self, star_number, row_number):
+        """Create star and place it in the row."""
+        star = Star(self)
+        star_width, star_height = star.rect.size
+        star.x = star_width + 2 * star_width * star_number
+        star.rect.x = star.x
+        star.rect.y = star.rect.height + 2 * star.rect.height * row_number
+        self.stars.add(star)
+       
 
 if __name__ == "__main__":
     # Create an instance of a game and run the game.
